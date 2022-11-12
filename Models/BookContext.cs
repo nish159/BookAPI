785a55c380cc475e4642dc170993c57fcb28174f
nish159 <77575013+nish159@users.noreply.github.com>
2022-11-11 19:29:33 -0500

    Add files via upload

---
+++ Models/BookContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Models
+{
+	public class BookContext : DbContext
+	{
+		// object is a configuration of the context; injected through dependency injection
+		public BookContext(DbContextOptions<BookContext> options)
+			:base(options)
+		{
+			// ensure database is created using dbcontext
+			Database.EnsureCreated();
+		}
+
+		// present a collection of books
+		public DbSet<Book> Books { get; set; }
+	}
+}
+
